e4cfb933d42c83ed9e97aa1ca94a4d06b61e2697
tnksn <67940119+tnksn@users.noreply.github.com>
2020-09-08 20:31:19 +0900

    課題提出
    Study.javaの作成

--- JavaStudy/2-4/src/study/Study.java
+++ JavaStudy/2-4/src/study/Study.java
@@ -15,6 +15,6 @@ public class Study {
          * ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。 また、課題の画像と同じ表示になるように
          * printFruitsの引数にはFruitsConstantsから呼び出した値を設定してください。
          */
-        Fruits.printFruits("レモン", 20);
+        Fruits.printFruits(FruitsConstants.FRUITS_LEMON_04, FruitsConstants.FRUITS_COUNT_20);
     }
 }
